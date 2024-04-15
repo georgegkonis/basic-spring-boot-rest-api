e9f1492451ea854c8920b973c3755a54fdbd5571
George Gkonis <georgegkonis@outlook.com>
2024-04-15 14:59:34 +0300

    feat: add `findByUsername` method in `UserRepository`

--- src/main/java/com/bioagg/repository/UserRepository.java
+++ src/main/java/com/bioagg/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.bioagg.repository;
 import com.bioagg.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 }
