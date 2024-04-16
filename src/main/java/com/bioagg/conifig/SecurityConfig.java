80708dba52bed1f34e0f4641f34c1566b57cd1de
George Gkonis <georgegkonis@outlook.com>
2024-04-16 09:59:00 +0300

    feat: add security

---
+++ src/main/java/com/bioagg/conifig/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.bioagg.conifig;
+
+import com.bioagg.filter.JwtRequestFilter;
+import com.bioagg.service.UserService;
+import com.bioagg.utility.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers("/api/auth/login").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.formLogin(withDefaults());
+        http.addFilterBefore(new JwtRequestFilter(userService, jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(
+            HttpSecurity http, BCryptPasswordEncoder passwordEncoder
+    ) throws Exception {
+        var authManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authManagerBuilder.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder);
+
+        return authManagerBuilder.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserService();
+    }
+}
---
+++ src/main/java/com/bioagg/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.bioagg.controller;
+
+import com.bioagg.dto.request.LoginRequest;
+import com.bioagg.dto.response.LoginResponse;
+import com.bioagg.utility.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authManager;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("login")
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody LoginRequest loginRequest
+    ) {
+        var authToken = new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password());
+        var authentication = authManager.authenticate(authToken);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        var jwt = jwtUtil.GenerateToken(loginRequest.username());
+        var response = new LoginResponse(jwt);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("logout")
+    public ResponseEntity<Void> logout() {
+        SecurityContextHolder.clearContext();
+
+        return ResponseEntity.noContent().build();
+    }
+}
