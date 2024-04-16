24ac778594db4849ce426b44577d5702fccc8f6f
George Gkonis <georgegkonis@outlook.com>
2024-04-16 09:56:37 +0300

    style: remove blank lines

--- src/main/java/com/bioagg/repository/CategoryRepository.java
+++ src/main/java/com/bioagg/repository/CategoryRepository.java
@@ -3,7 +3,4 @@ package com.bioagg.repository;
 import com.bioagg.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-
-}
+public interface CategoryRepository extends JpaRepository<Category, Long> {}
--- src/main/java/com/bioagg/repository/ProductRepository.java
+++ src/main/java/com/bioagg/repository/ProductRepository.java
@@ -3,5 +3,4 @@ package com.bioagg.repository;
 import com.bioagg.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
+public interface ProductRepository extends JpaRepository<Product, Long> {}
